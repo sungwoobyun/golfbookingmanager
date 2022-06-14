557a48ba611e53fed96c24c3e7ca7fbffc231d73
autodoc <autodoc@daum.net>
2022-06-14 13:24:00 +0900

    ㅔ네번째 커밋

---
+++ booking/urls.py
@@ -0,0 +1,42 @@
+from django.urls import path
+from .views import base_views, question_views, answer_views
+from . import views
+
+app_name = 'booking'
+urlpatterns = [
+    # base_views.py
+    path('', base_views.index, name='index'),
+    path('<int:question_id>/', base_views.detail, name='detail'),
+
+    # question_views.py
+    path('question/create/', question_views.question_create, name='question_create'),
+    path('question/modify/<int:question_id>/', question_views.question_modify, name='question_modify'),
+    path('question/delete/<int:question_id>/', question_views.question_delete, name='question_delete'),
+
+    # answer_views.py
+    path('answer/create/<int:question_id>/', answer_views.answer_create, name='answer_create'),
+    path('answer/modify/<int:answer_id>/', answer_views.answer_modify, name='answer_modify'),
+    path('answer/delete/<int:answer_id>/', answer_views.answer_delete, name='answer_delete'),
+
+    path('question/vote/<int:question_id>/', question_views.question_vote, name='question_vote'),
+    path('answer/vote/<int:answer_id>/', answer_views.answer_vote, name='answer_vote'),
+    
+    # path('', views.index, name='index'),
+    # path('<int:question_id>/', views.detail, name='detail'),
+    # # path('', views.index),
+    # # path('<int:question_id>/', views.detail),
+    # path('answer/create/<int:question_id>/', views.answer_create, name='answer_create'),
+    # path('question/create/', views.question_create, name='question_create'),
+    # path('question/modify/<int:question_id>/', views.question_modify, name='question_modify'),
+    # path('question/delete/<int:question_id>/', views.question_delete, name='question_delete'),
+    # path('answer/modify/<int:answer_id>/', views.answer_modify, name='answer_modify'),
+    # path('answer/delete/<int:answer_id>/', views.answer_delete, name='answer_delete'),
+]
+
+
+# 아래는 generic view 적용 시를 기준함(views.py와 함께 수정 필요
+# app_name = 'booking'
+# urlpatterns = [
+#     path('', views.IndexView.as_view()),
+#     path('<int:pk>/', views.DetailView.as_view()),
+# ]
---
+++ booking/views-orig.py
@@ -0,0 +1,125 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Question, Answer
+from django.http import HttpResponseNotAllowed
+from .forms import QuestionForm, AnswerForm
+from django.core.paginator import Paginator 
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+
+def index(request):
+    page = request.GET.get('page', '1')  # 페이지
+    question_list = Question.objects.order_by('-create_date')
+    paginator = Paginator(question_list, 10)  # 페이지당 10개씩 보여주기
+    page_obj = paginator.get_page(page)
+    context = {'question_list': page_obj}
+    # context = {'question_list': question_list}
+    return render(request, 'booking/question_list.html', context)
+
+def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question': question}
+    return render(request, 'booking/question_detail.html', context)
+
+# 아래는 모두 generic view임
+# class IndexView(generic.ListView):
+#     def get_queryset(self):
+#         return Question.objects.order_by('-create_date')
+
+# class DetailView(generic.DetailView):
+#     model = Question
+
+
+# answer로 직접 읽어오는 방법이 주석처리됨
+# from .models import Question, answer
+@login_required(login_url='common:login')
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('booking:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'booking/question_detail.html', context)
+
+    # question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    # # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # # answer.save()
+    # return redirect('booking:detail', question_id=question.id)
+
+@login_required(login_url='common:login')
+def question_create(request):
+    if request.method == 'POST':
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('booking:index')
+    else:
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'booking/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('booking:detail', question_id=question.id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('booking:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'booking/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('booking:detail', question_id=question.id)
+    question.delete()
+    return redirect('booking:index')
+
+@login_required(login_url='common:login')
+def answer_modify(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('booking:detail', question_id=answer.question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST, instance=answer)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.modify_date = timezone.now()
+            answer.save()
+            return redirect('booking:detail', question_id=answer.question.id)
+    else:
+        form = AnswerForm(instance=answer)
+    context = {'answer': answer, 'form': form}
+    return render(request, 'booking/answer_form.html', context)
+
+@login_required(login_url='common:login')
+def answer_delete(request, answer_id):
+    answer = get_object_or_404(Answer, pk=answer_id)
+    if request.user != answer.author:
+        messages.error(request, '삭제권한이 없습니다')
+    else:
+        answer.delete()
+    return redirect('booking:detail', question_id=answer.question.id)
